ad298ef8fcd1922d7b0955b9b8ed35679f61389e
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-21 10:06:42 +0100

    Kommenterede i GUIInterface og rettede i featurelisten.

--- Flybooking/src/flybooking/GUIInterface.java
+++ Flybooking/src/flybooking/GUIInterface.java
@@ -2,7 +2,7 @@
 package flybooking;
 
 /**
- *
+ * An interface for creating a GUI.
  * @author Anders Wind Steffensen, Christoffer Forup & Anders Fischer-Nielsen
  */
 public interface GUIInterface {
