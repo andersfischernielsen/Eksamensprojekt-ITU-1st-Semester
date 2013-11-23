c5645408729d5c5543c29a88d1702794dd370a27
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-23 16:14:38 +0100

    Oprettede et simpelt interface til at tegne flysæderne.

---
+++ Flybooking/src/flybooking/PlaneGraphicsComponent.java
@@ -0,0 +1,24 @@
+
+package flybooking;
+
+import java.awt.Component;
+
+/**
+ * A custom graphics component that can draw available seats on a plane. 
+ * @author Anders
+ */
+public class PlaneGraphicsComponent extends Component {
+    private int rows;
+    private int columns;
+    
+    public boolean getAvalable(Seat seat) {
+        return seat.getAvailability();
+    }
+    
+    public void drawSeats(Plane plane) {
+        rows = plane.getRows();
+        columns = plane.getColumns();
+        
+        //KODE TIL AT TEGNE SÆDER HER.
+    }
+}
