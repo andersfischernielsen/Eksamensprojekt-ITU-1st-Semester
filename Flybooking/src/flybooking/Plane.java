cab29d7403cb2014139d1a476e9d06d6dfe09a62
Awia00 <awia00@gmail.com>
2013-11-25 17:36:49 +0100

    Ændringer til Plane: En initiator metode som laver et seat for hver col og hver row, og som i samme omgang laver et sæde id En sæde id generator som laver et id ud fra col og row. Mangler dog dokumentation

--- Flybooking/src/flybooking/Plane.java
+++ Flybooking/src/flybooking/Plane.java
@@ -10,8 +10,7 @@ public class Plane
 
     private final String ID;
     private final int numberOfSeats;
-    public final int rows;
-    public final int columns;
+    public final int rows, columns;
     private final Seat[][] seats;
 
     public Plane(String ID, int numberOfSeats, int rows, int columns)
@@ -22,6 +21,48 @@ public class Plane
         this.columns = columns;
         seats = new Seat[columns][rows];
         numberOfSeats = columns * rows;
+    }
+
+    /**
+     * Initiates all the seats in the plane
+     */
+    private void initiate()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                seats[i][j] = new Seat(SeatIDGenerator(i, j), 0);
+            }
+        }
+    }
+
+    public String SeatIDGenerator(int col, int row)
+    {
+        int theCol = col+1;
+        String seatIDString = "" + theCol;
+        switch (row)
+        {
+            case 0:
+                seatIDString += "A";
+                break;
+            case 1:
+                seatIDString += "B";
+                break;
+            case 2:
+                seatIDString += "C";
+                break;
+            case 3:
+                seatIDString += "D";
+                break;
+            case 4:
+                seatIDString += "E";
+                break;
+            case 5:
+                seatIDString += "F";
+                break;
+        }
+        return seatIDString;
     }
 
     /**
@@ -63,17 +104,17 @@ public class Plane
     /**
      * Return a seat with the given seat ID. If no seat was found, return null.
      *
-     * @param ID The seat ID to get.
+     * @param seatID The seat ID to get.
      *
      * @return The seat with the given seat ID. If not found, return null.
      */
-    public Seat getSeat(int seatID)
+    public Seat getSeat(String seatID)
     {
         for (int i = 0; i < columns; i++)
         {
             for (int j = 0; i < rows; j++)
             {
-                if (seats[i][j] != null && seats[i][j].getID() == seatID)
+                if (seats[i][j] != null && seats[i][j].getID().equals(seatID))
                 {
                     return seats[i][j];
                 }
@@ -93,13 +134,13 @@ public class Plane
      * @return The availability of the seat with the given seat ID. If not
      *         found, return false.
      */
-    public boolean getSeatAvailability(int seatID)
+    public boolean getSeatAvailability(String seatID)
     {
         for (int i = 0; i < columns; i++)
         {
             for (int j = 0; i < rows; j++)
             {
-                if (seats[i][j] != null && seats[i][j].getID() == seatID)
+                if (seats[i][j] != null && seats[i][j].getID().equals(seatID))
                 {
                     return seats[i][j].getAvailability();
                 }
@@ -114,13 +155,13 @@ public class Plane
      * @param seatID       The seat ID to change
      * @param availability Whether the seat is available or not.
      */
-    public void setSeatAvailability(int seatID, boolean availability)
+    public void setSeatAvailability(String seatID, boolean availability)
     {
         for (int i = 0; i < columns; i++)
         {
             for (int j = 0; i < rows; j++)
             {
-                if (seats[i][j] != null && seats[i][j].getID() == seatID)
+                if (seats[i][j] != null && seats[i][j].getID().equals(seatID))
                 {
                     seats[i][j].setAvailability(availability);
                 }
