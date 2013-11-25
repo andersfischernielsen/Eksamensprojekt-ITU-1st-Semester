9f8fe81df7881d487f7c197a583ac4b1e2e2e8c5
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-25 15:48:46 +0100

    Et forsøg på implementation. Det ligner lort.

--- Flybooking/src/flybooking/GUI/NewReservationFrame.java
+++ Flybooking/src/flybooking/GUI/NewReservationFrame.java
@@ -1,7 +1,8 @@
 
 package flybooking.GUI;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *
@@ -9,4 +10,64 @@ import javax.swing.JFrame;
  */
 public class NewReservationFrame extends JFrame {
 
+    private JComboBox dateDropdown, peopleDropdown, startDestDropdown, endDestDropdown;
+    private JButton searchButton;
+    private JCheckBox checkbox;
+    private FlightList flightList;
+    private Container content, topContainer, bottomContainer, filler;
+    private JLabel departureLabel, peopleLabel, startLabel, endLabel;
+
+    public NewReservationFrame() throws HeadlessException
+    {
+        String[] testStrings = {"So wow", "much wow", "so string", "much dropdown"};
+         
+        setDefaultCloseOperation(NewReservationFrame.EXIT_ON_CLOSE);
+        content = getContentPane();
+        topContainer = new Container();
+        topContainer.setPreferredSize(new Dimension(400, 300));
+        topContainer.setLayout(new GridLayout(3, 3));
+        bottomContainer = new Container();
+        //bottomContainer.add(flightList);
+        content.add(topContainer, BorderLayout.NORTH);
+        filler = new Container();
+        
+        departureLabel = new JLabel("Departure date:");
+        dateDropdown = new JComboBox(testStrings);
+        topContainer.add(departureLabel);
+        topContainer.add(dateDropdown);
+
+        peopleLabel = new JLabel("People:");
+        peopleDropdown = new JComboBox(testStrings);
+        topContainer.add(peopleLabel);
+        topContainer.add(peopleDropdown);
+
+        startLabel = new JLabel("Start destination:");
+        startDestDropdown = new JComboBox(testStrings);
+        topContainer.add(startLabel);
+        topContainer.add(startDestDropdown);
+        
+        topContainer.add(filler);
+        
+        checkbox = new JCheckBox("Next to: ");
+        topContainer.add(checkbox);
+        
+        endLabel = new JLabel("End destination:");
+        endDestDropdown = new JComboBox(testStrings);
+        topContainer.add(endLabel);
+        topContainer.add(endDestDropdown);
+        
+        topContainer.add(filler);
+        topContainer.add(filler);
+        
+        searchButton = new JButton("Search");
+        topContainer.add(searchButton);
+        
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args)
+    {
+        new NewReservationFrame();
+    }
 }
