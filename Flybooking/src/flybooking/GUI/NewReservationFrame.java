2b19b16a21cbb8d6f150ddcd5822f8f1c31b7d54
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-25 19:13:17 +0100

    Rettede mere layout og fjernede lidt kodeduplikering.

--- Flybooking/src/flybooking/GUI/NewReservationFrame.java
+++ Flybooking/src/flybooking/GUI/NewReservationFrame.java
@@ -17,59 +17,129 @@ public class NewReservationFrame extends JFrame {
     private JButton searchButton;
     private JCheckBox checkbox;
     private FlightList flightList;
-    private Container content, topContainer, bottomContainer, filler;
+    private Container content, topContainer, bottomContainer,
+            top1x1Container, top1x2Container, top1x3Container,
+            top2x1Container, top2x2Container, top2x3Container,
+            top3x1Container, top3x2Container, top3x3Container;
+    private ArrayList<Container> topContainers;
     private JLabel departureLabel, peopleLabel, startLabel, endLabel;
+    private DatabaseInterface flightDatabase;
 
-    public NewReservationFrame() throws HeadlessException
+    /**
+     * Get an instance of the Frame. (Singleton)
+     *
+     * @return An instance of the frame.
+     */
+    public static NewReservationFrame getInstance()
     {
-        String[] testStrings = {"So wow", "much wow", "so string", "much dropdown"};
-         
-        setDefaultCloseOperation(NewReservationFrame.EXIT_ON_CLOSE);
-        content = getContentPane();
-        topContainer = new Container();
-        topContainer.setPreferredSize(new Dimension(400, 300));
-        topContainer.setLayout(new GridLayout(3, 3));
-        bottomContainer = new Container();
-        content.add(topContainer, BorderLayout.NORTH);
-        filler = new Container();
-        
-        departureLabel = new JLabel("Departure date:");
-        dateDropdown = new JComboBox(testStrings);
-        topContainer.add(departureLabel);
-        topContainer.add(dateDropdown);
+        NewReservationFrame instance = null;
 
-        peopleLabel = new JLabel("People:");
-        peopleDropdown = new JComboBox(testStrings);
-        topContainer.add(peopleLabel);
-        topContainer.add(peopleDropdown);
+        if (instance == null) {
+            instance = new NewReservationFrame();
+        }
 
-        startLabel = new JLabel("Start destination:");
-        startDestDropdown = new JComboBox(testStrings);
-        topContainer.add(startLabel);
-        topContainer.add(startDestDropdown);
-        
-        topContainer.add(filler);
-        
-        checkbox = new JCheckBox("Next to: ");
-        topContainer.add(checkbox);
-        
-        endLabel = new JLabel("End destination:");
-        endDestDropdown = new JComboBox(testStrings);
-        topContainer.add(endLabel);
-        topContainer.add(endDestDropdown);
-        
-        topContainer.add(filler);
-        topContainer.add(filler);
-        
-        searchButton = new JButton("Search");
-        topContainer.add(searchButton);
-        
-        pack();
-        setVisible(true);
+        return instance;
     }
 
     public static void main(String[] args)
     {
-        new NewReservationFrame();
+        NewReservationFrame.getInstance();
+    }
+
+    /**
+     * Create a frame for finding and creating reservations.
+     *
+     * @throws HeadlessException
+     */
+    private NewReservationFrame() throws HeadlessException
+    {
+        setDefaultCloseOperation(NewReservationFrame.EXIT_ON_CLOSE);
+        setTitle("New Reservation");
+        content = getContentPane();
+        topContainers = new ArrayList<>();
+
+        drawTopContent();
+        drawBottomContent();
+
+        pack();
+        setVisible(true);
+    }
+
+    /**
+     * Draw the top part of the window, containing input and search buttons.
+     */
+    private void drawTopContent()
+    {
+        String[] testStrings = {"One, two", "three, testing", "Is this on?", "Copenhagen"};
+
+        //Create an empty container and set the layout to a GridLayout.
+        topContainer = new Container();
+        topContainer.setLayout(new GridLayout(3, 3));
+        
+        //Add all of the containers for each grid to an ArrayList and initialize
+        //them.
+        topContainers.add(top1x1Container = new Container());
+        topContainers.add(top1x2Container = new Container());
+        topContainers.add(top1x3Container = new Container());
+        topContainers.add(top2x1Container = new Container());
+        topContainers.add(top2x2Container = new Container());
+        topContainers.add(top2x3Container = new Container());
+        topContainers.add(top3x1Container = new Container());
+        topContainers.add(top3x2Container = new Container());
+        topContainers.add(top3x3Container = new Container());
+
+        //Add all of the labels to each grid container.
+        departureLabel = new JLabel("Departure date:");
+        dateDropdown = new JComboBox(testStrings);
+        top1x1Container.add(departureLabel);
+        top1x1Container.add(dateDropdown);
+
+        peopleLabel = new JLabel("People:");
+        peopleDropdown = new JComboBox(testStrings);
+        top1x2Container.add(peopleLabel);
+        top1x2Container.add(peopleDropdown);
+
+        startLabel = new JLabel("Start destination:");
+        startDestDropdown = new JComboBox(testStrings);
+        top1x3Container.add(startLabel);
+        top1x3Container.add(startDestDropdown);
+
+        checkbox = new JCheckBox("Next to: ");
+        checkbox.setHorizontalTextPosition(SwingConstants.LEFT);
+        top2x2Container.add(checkbox);
+
+        endLabel = new JLabel("End destination:");
+        endDestDropdown = new JComboBox(testStrings);
+        top2x3Container.add(endLabel);
+        top2x3Container.add(endDestDropdown);
+
+        searchButton = new JButton("Search");
+        top3x3Container.add(searchButton);
+
+        //Go through all of the grid containers and set their layout, size and
+        //finally add them to the topContainer.
+        for (Container cont : topContainers) {
+            cont.setLayout(new FlowLayout());
+            cont.setPreferredSize(new Dimension(150, 100));
+            topContainer.add(cont);
+        }
+        
+        //Add the finished Container to the frame.
+        content.add(topContainer, BorderLayout.NORTH);
+    }
+
+    /**
+     * Draw the bottom part of the window, containing flights found.
+     */
+    private void drawBottomContent()
+    {
+        bottomContainer = new Container();
+        
+        JButton testButton = new JButton("Testing!") ;
+        Container tempCont = new Container();
+        tempCont.add(testButton);
+        tempCont.setSize(new Dimension(400, 300));
+        bottomContainer.add(tempCont);
+        content.add(bottomContainer, BorderLayout.SOUTH);
     }
 }
