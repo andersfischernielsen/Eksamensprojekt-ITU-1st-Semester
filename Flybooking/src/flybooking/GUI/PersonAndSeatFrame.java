c35ecf78e0a352179ba788a2007eaf4d72dfa8da
Awia00 <awia00@gmail.com>
2013-11-25 21:34:37 +0100

    PersonAndSeatFrame, rettet yderligere til. Her kan også ses Den nye GraphicsComponent in-action for første gang

--- Flybooking/src/flybooking/GUI/PersonAndSeatFrame.java
+++ Flybooking/src/flybooking/GUI/PersonAndSeatFrame.java
@@ -12,20 +12,22 @@ public class PersonAndSeatFrame extends JFrame
 {
 
     // denne er bare til test.
-    Plane planeToDraw = new Plane("5eerrg2", 6, 20);
-    
+    private Plane planeToDraw = new Plane("5eerrg2", 6, 20);
 
+    GraphicsComponent graphics;
     private int amtOfPersons;
     private JComboBox personComboBox, ageGroupComboBox;
 
     public PersonAndSeatFrame(int amtOfPersons) throws HeadlessException
     {
+        graphics = new GraphicsComponent();
+
         planeToDraw.setSeatAvailability("5A", false);
         planeToDraw.setSeatAvailability("10B", false);
         planeToDraw.setSeatAvailability("6F", false);
         planeToDraw.setSeatAvailability("6E", false);
         planeToDraw.setSeatAvailability("6C", false);
-        
+
         this.amtOfPersons = amtOfPersons;
         setTitle("Pick seats and passengers");
         setPreferredSize(new Dimension(800, 600));
@@ -36,22 +38,28 @@ public class PersonAndSeatFrame extends JFrame
         // laver første opdeling
         JPanel botPanel = new JPanel();
         JPanel topPanel = new JPanel();
-            // TOP PANEL's Panels
-                // top left panels
+        topPanel.setLayout(new BorderLayout());
+        // TOP PANEL's Panels
+        // top left panel
         JPanel topLeftPanel = new JPanel();
         topLeftPanel.setLayout(new BoxLayout(topLeftPanel, BoxLayout.PAGE_AXIS));
         topPanel.add(topLeftPanel, BorderLayout.WEST);
-                // top right panels
+        // top right panel
         JPanel topRightPanel = new JPanel();
         topRightPanel.setLayout(new BoxLayout(topRightPanel, BoxLayout.PAGE_AXIS));
         topPanel.add(topRightPanel, BorderLayout.EAST);
-                // top bot panels
+        // top bot panel
         JPanel topBotPanel = new JPanel();
         topPanel.add(topBotPanel, BorderLayout.SOUTH);
+        // Top top panel
+        JPanel topTopPanel = new JPanel();
+        topPanel.add(topTopPanel, BorderLayout.NORTH);
+
         //tilføjer panels til main panel
-        mainPanel.add(botPanel, BorderLayout.SOUTH);
+        mainPanel.add(botPanel, BorderLayout.CENTER);
         mainPanel.add(topPanel, BorderLayout.NORTH);
 
+        // -------------------- CONTENT VENSTRE DEL TOP --------------------
         // FirstName text
         JTextField firstNameText = new JTextField("First name");
         firstNameText.setEditable(false);
@@ -60,8 +68,8 @@ public class PersonAndSeatFrame extends JFrame
         JTextField firstNameTextField = new JTextField("Put last name here...");
         firstNameTextField.setForeground(Color.gray);
         topLeftPanel.add(firstNameTextField);
-        topLeftPanel.add(Box.createRigidArea(new Dimension(0,5)));
-        
+        topLeftPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+
         // LastName text
         JTextField lastNameText = new JTextField("Last name");
         lastNameText.setEditable(false);
@@ -70,7 +78,25 @@ public class PersonAndSeatFrame extends JFrame
         JTextField lastNameTextField = new JTextField("Put last name here...");
         lastNameTextField.setForeground(Color.gray);
         topLeftPanel.add(lastNameTextField);
-        
+
+        // AgeGroup combobox
+        String[] ageGroups =
+        {
+            "Children",
+            "Adult",
+            "Elder"
+        };
+        ageGroupComboBox = new JComboBox(ageGroups);
+        topLeftPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        topLeftPanel.add(ageGroupComboBox);
+
+        // Persons ComboBox
+        personComboBox = new JComboBox();
+        updatePersonComboBox();
+        topLeftPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        topLeftPanel.add(personComboBox);
+
+        // -------------------- CONTENT HØJRE DEL TOP ----------------------
         // Address text
         JTextField AddressText = new JTextField("Address");
         AddressText.setEditable(false);
@@ -79,31 +105,28 @@ public class PersonAndSeatFrame extends JFrame
         JTextField streetTextField = new JTextField("Street...");
         streetTextField.setForeground(Color.gray);
         topRightPanel.add(streetTextField);
-        topRightPanel.add(Box.createRigidArea(new Dimension(0,25)));
+        topRightPanel.add(Box.createRigidArea(new Dimension(0, 25)));
         // City textField
         JTextField cityTextField = new JTextField("ZIP, City, Country");
         cityTextField.setForeground(Color.gray);
         topRightPanel.add(cityTextField);
-        topRightPanel.add(Box.createRigidArea(new Dimension(0,0)));
+        topRightPanel.add(Box.createRigidArea(new Dimension(0, 90)));
 
-        // AgeGroup combobox
-        JMenuItem[] ageGroups =
-        {
-            new JMenuItem("Children"),
-            new JMenuItem("Adult"),
-            new JMenuItem("Elder")
-        };
-        ageGroupComboBox = new JComboBox(ageGroups);
+        // -------------------- CONTENT TOP TOP ----------------------
+        pack();
+        topTopPanel.add(Box.createRigidArea(new Dimension(0, 30)));
+        topTopPanel.add(graphics.paintHeader(20, 760));
+        topTopPanel.add(Box.createRigidArea(new Dimension(0, 50)));
 
-        // Persons ComboBox
-        personComboBox = new JComboBox();
-        updatePersonComboBox();
+        // -------------------- CONTENT TOP BUND ----------------------
+        
+        topBotPanel.add(Box.createRigidArea(new Dimension(0, 50)));
+        
+        // -------------------- CONTENT BUNDEN ----------------------
+       // the plane drawing
+        botPanel.add(graphics.paintPlaneSeats(planeToDraw));
+        //PlaneDrawing.paintPlaneSeats(planeToDraw);
 
-        // the plane drawing
-        PlaneGraphicsComponent PlaneDrawing = new PlaneGraphicsComponent(planeToDraw);
-        botPanel.add(PlaneDrawing);
-        PlaneDrawing.repaint();
-                
         pack();
         setVisible(true);
     }
@@ -112,9 +135,9 @@ public class PersonAndSeatFrame extends JFrame
     {
         for (int i = 1; i <= this.amtOfPersons; i++)
         {
-            personComboBox.addItem(new JMenuItem("Person " + (i)));
+            personComboBox.addItem("Person " + (i));
         }
-        personComboBox.addItem(new JMenuItem("Add another person"));
+        personComboBox.addItem("Add another person");
 
     }
 
