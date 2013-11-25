e57218e2e23a3b0b82142c174395ee7b1c827725
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-25 13:26:08 +0100

    Lavede StartFrame efter meget bøvl med Layout Managers.

--- Flybooking/src/flybooking/GUI/StartFrame.java
+++ Flybooking/src/flybooking/GUI/StartFrame.java
@@ -1,7 +1,8 @@
 
 package flybooking.GUI;
 
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *
@@ -9,4 +10,26 @@ import javax.swing.JFrame;
  */
 public class StartFrame extends JFrame {
 
+    Container content;
+    Container empty;
+    Container innerCont;
+    JButton newBookingButton;
+    JButton editBookingButton;
+
+    public StartFrame() throws HeadlessException
+    {
+        setTitle("Flybooking");
+        content = this.getContentPane();
+        innerCont = new Container();
+        empty = new Container();
+        content.add(innerCont);
+        innerCont.setLayout(new FlowLayout());
+        newBookingButton = new JButton("New booking");
+        editBookingButton = new JButton("Edit booking");
+        innerCont.add(newBookingButton);
+        innerCont.add(editBookingButton);
+
+        this.pack();
+        this.setVisible(true);
+    }
 }
