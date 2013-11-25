0661445f9f73d3abfcebf01cf707e1d0075b78d5
Awia00 <awia00@gmail.com>
2013-11-25 17:35:13 +0100

    GUI Arbejde på PersonsAndSeats og på graphics componentet der tegner flyet

--- Flybooking/src/flybooking/GUI/PersonAndSeatFrame.java
+++ Flybooking/src/flybooking/GUI/PersonAndSeatFrame.java
@@ -1,4 +1,3 @@
-
 package flybooking.GUI;
 
 import java.awt.*;
@@ -8,11 +7,76 @@ import javax.swing.*;
  *
  * @author Anders
  */
-public class PersonAndSeatFrame extends JFrame {
+public class PersonAndSeatFrame extends JFrame
+{
 
-    public PersonAndSeatFrame() throws HeadlessException
+    private int amtOfPersons;
+    private JComboBox personComboBox, ageGroupComboBox;
+
+    public PersonAndSeatFrame(int amtOfPersons) throws HeadlessException
     {
+        this.amtOfPersons = amtOfPersons;
         setTitle("Pick seats and passengers");
-        
+        this.setPreferredSize(new Dimension(800, 600));
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // ved godt det ikke skal være sådan.
+        Container mainContainer = new Container();
+
+        // FirstName text
+        JTextField firstNameText = new JTextField("First name");
+        firstNameText.setEditable(false);
+        // FirstName textField
+        JTextField firstNameTextField = new JTextField("Put last name here...");
+        firstNameTextField.setForeground(Color.gray);
+
+        // LastName text
+        JTextField lastNameText = new JTextField("Last name");
+        lastNameText.setEditable(false);
+        // LastName textField
+        JTextField lastNameTextField = new JTextField("Put last name here...");
+        lastNameTextField.setForeground(Color.gray);
+
+        // Address text
+        JTextField AddressText = new JTextField("Address");
+        lastNameText.setEditable(false);
+        // Street textField
+        JTextField streetTextField = new JTextField("Street...");
+        streetTextField.setForeground(Color.gray);
+        // City textField
+        JTextField cityTextField = new JTextField("ZIP, City, Country");
+        cityTextField.setForeground(Color.gray);
+
+        // AgeGroup combobox
+        JMenuItem[] ageGroups =
+        {
+            new JMenuItem("Children"),
+            new JMenuItem("Adult"),
+            new JMenuItem("Elder")
+        };
+        ageGroupComboBox = new JComboBox(ageGroups);
+
+        // Persons ComboBox
+        personComboBox = new JComboBox();
+        updatePersonComboBox();
+
+        // the plane drawing
+        Canvas PlaneDrawing;
+
+        pack();
+        setVisible(true);
+    }
+
+    private void updatePersonComboBox()
+    {
+        for (int i = 1; i <= this.amtOfPersons; i++)
+        {
+            personComboBox.addItem(new JMenuItem("Person " + (i)));
+        }
+        personComboBox.addItem(new JMenuItem("Add another person"));
+
+    }
+
+    public static void main(String[] args)
+    {
+        new PersonAndSeatFrame(3);
     }
 }
