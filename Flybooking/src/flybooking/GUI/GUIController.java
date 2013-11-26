efff31008beaac95bceb8f759e56ab68a3849d02
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-26 15:55:29 +0100

    Lavede tom GUIController-klasse.

---
+++ Flybooking/src/flybooking/GUI/GUIController.java
@@ -0,0 +1,37 @@
+
+package flybooking.GUI;
+
+import flybooking.*;
+
+/**
+ *
+ * @author Anders
+ */
+public class GUIController implements GUIInterface {
+
+    @Override
+    public void createStartFrame()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createNewReservationFrame(Controller controller)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createEditreservationFrame()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createPersonAndSeatFrame()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+}
