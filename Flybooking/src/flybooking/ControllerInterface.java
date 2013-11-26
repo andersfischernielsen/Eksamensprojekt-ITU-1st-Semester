8d73bb7672ec8f4d140ad344c0b4cea747fc6535
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-26 18:39:53 +0100

    Oprettet metoder til at hente og tælle antal tilgængelige flights.

--- Flybooking/src/flybooking/ControllerInterface.java
+++ Flybooking/src/flybooking/ControllerInterface.java
@@ -11,7 +11,6 @@ public interface ControllerInterface {
 
     //(FISCHER) JEG HAR LAVET NOGLE GENERELLE METODER. DER MANGLER EN DEL, 
     //OG DE SKAL SIKKERT ÆNDRES/UDDYBES, MEN SÅ HAR VI ET STED AT STARTE. 
-    
     /**
      * Begin the process of creating a new reservation.
      */
@@ -21,24 +20,28 @@ public interface ControllerInterface {
 
     /**
      * Save the reservation when finished.
+     *
      * @param database The database to save the reservation to.
      */
     void saveReservation(DatabaseInterface database);
 
     /**
      * Get a specific reservation.
+     *
      * @param database The database to get the reservation from.
      */
     void getReservation(DatabaseInterface database);
 
     /**
      * Delete a reservation.
-     * @param database The database to delete the reservation from. 
+     *
+     * @param database The database to delete the reservation from.
      */
     void deleteReservation(DatabaseInterface database);
 
     /**
      * Print the receipt of the reservation.
+     *
      * @param reservation The reservation to print.
      * @param printer The printer to use to print.
      */
@@ -46,6 +49,7 @@ public interface ControllerInterface {
 
     /**
      * Get the total price of the reservation.
+     *
      * @param calculator The calculator to use
      * @param reservation The reservation to calculate the price for.
      * @return The price for the reservation
@@ -54,19 +58,22 @@ public interface ControllerInterface {
 
     /**
      * Find something.
+     *
      * @param database The database to search in.
      */
     void search(DatabaseInterface database); //SEARCH BLIVER MERE AVANCERET AT LAVE, SÅ VI SKAL SIKKERT HAVE EN DEL ANDRE SØGEMETODER.
 
     /**
      * Checks to see if anyone is next to the specified person.
+     *
      * @param person The person to check for neighbors.
      * @return Whether there are any neighbors next to the specified person.
      */
     boolean hasPersonNextTo(Person person);
-    
+
     /**
      * Get the person next to the specified person.
+     *
      * @param person The person to check for neighbors.
      * @return The person next to the specified person.
      */
@@ -76,4 +83,18 @@ public interface ControllerInterface {
     //ELLER VI GODT KAN KALDE F.EKS. addPerson() PÅ RESERVATIONEN ISTEDET. 
     //DET MEST ELEGANTE VILLE VÆRE AT CONTROLLEREN STYRER ALTING, MEN SÅ FÅR VI BARE EN DEL METODER,
     //DER BARE KALDER EN TILSVARENDE METODE PÅ ET ANDET OBJEKT (F.EKS. addPerson()).
+    
+    /**
+     * Get the number of flights available from the database.
+     *
+     * @param database The database to use.
+     * @return The number of available flights.
+     */
+    int getNumberOfFlights();
+    
+    /**
+     * Get all flights from the database.
+     * @return An array of flights.
+     */
+    Flight[] getAllFlights();
 }
