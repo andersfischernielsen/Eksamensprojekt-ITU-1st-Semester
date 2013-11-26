3006097a3d541333d462a840c7325ad6e53a0c02
Anders Fischer-Nielsen <andersfischern@me.com>
2013-11-26 15:54:09 +0100

    Videresending af Controller.

--- Flybooking/src/flybooking/GUI/GUIInterface.java
+++ Flybooking/src/flybooking/GUI/GUIInterface.java
@@ -1,13 +1,15 @@
 
 package flybooking.GUI;
 
+import flybooking.*;
+
 /**
  * An interface for creating a GUI.
  * @author Anders Wind Steffensen, Christoffer Forup & Anders Fischer-Nielsen
  */
 public interface GUIInterface {
     void createStartFrame();
-    void createReservationFrame();
+    void createNewReservationFrame(Controller controller);
     void createEditreservationFrame();
     void createPersonAndSeatFrame();
 }
