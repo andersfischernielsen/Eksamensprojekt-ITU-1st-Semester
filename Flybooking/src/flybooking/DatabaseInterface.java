6e445f2aa9767186d4757eacf480141690643c1f
Anders Wind <awia00@gmail.com>
2013-11-28 12:18:41 +0100

    Database interface work

--- Flybooking/src/flybooking/DatabaseInterface.java
+++ Flybooking/src/flybooking/DatabaseInterface.java
@@ -10,84 +10,109 @@ import java.util.Date;
  */
 public interface DatabaseInterface
 {
-    
+
     /**
-     * returns the plane with ID PlaneID (needs to implement something with seats)
+     * returns the plane with ID PlaneID (needs to implement something with
+     * seats)
+     *
      * @param PlaneID
-     * @return 
-     * @throws java.sql.SQLException 
+     *
+     * @return
+     *
+     * @throws java.sql.SQLException
      */
-    Plane getPlane(String PlaneID)throws SQLException; 
+    Plane getPlane(String PlaneID) throws SQLException;
 
     /**
-     * returns an array of seats which are not available.
-     * @param flight
-     * @param persons
-     * @return 
-     */
-    ArrayList<Seat> getTakenSeats(Flight flight, Person[] persons);
-
-    /** 
      * Returns an ArrayList of flights that match the specified search terms.
-     * @param departureDate The date to depart.
+     *
+     * @param departureDate    The date to depart.
      * @param startDestination The start destination for the flight.
-     * @param endDestination The end destination for the flight.
+     * @param endDestination   The end destination for the flight.
+     *
      * @return An array of flights that match the specified parameters.
      */
-    ArrayList<Flight> getFlight(Date departureDate, 
-                String startDestination, String endDestination)
-                throws SQLException;
+    ArrayList<Flight> getFlight(Date departureDate,
+                                String startDestination, String endDestination)
+            throws SQLException;
 
     /**
      * Returns an arrayList of reservations which respects the parameters.
+     *
      * @param ReservationID
      * @param CPR
-     * @return 
+     *
+     * @return
      */
     ArrayList<ReservationInterface> getReservations(String reservationID, String CPR);
 
     /**
      * Enten den her
+     *
      * @param flight
      * @param persons
-     * @param Price HUSK AT ÆNDRE DEN TIL LILLE SKRIFT!
+     * @param Price   HUSK AT ÆNDRE DEN TIL LILLE SKRIFT!
      * @param CPR
      */
     void newReservation(Flight flight, Person[] persons, String CPR, double Price);
 
     /**
-     * eller den 
+     * eller den
+     *
      * @param reservationToMake
      */
     void newReservation(Reservation reservationToMake);
 
     /**
      * remove the reservation with ID reservationID
+     *
      * @param reservationID
      */
     void removeReservation(String reservationID) throws SQLException;
-    
+
     /**
      * Adds a person to a reservation.
-     * @param reservationID The reservation in the database to add to.
-     * @param personToAdd The person to add to the reservation.
-     * @param reservationSpot The spot in the reservation to add the person (first, second etc.)
+     *
+     * @param reservationID   The reservation in the database to add to.
+     * @param personToAdd     The person to add to the reservation.
+     * @param reservationSpot The spot in the reservation to add the person
+     *                        (first, second etc.)
+     *
      * @throws java.sql.SQLException
      */
     void addPersonToReservation(String reservationID, Person personToAdd, String reservationSpot) throws SQLException;
-    
+
     /**
      * Get all the available flights as an ArrayList.
+     *
      * @return An ArrayList of all the available flights.
+     *
      * @throws java.sql.SQLException An SQLException if something went wrong.
      */
     ArrayList<String> getAirportCitiesAsStrings() throws SQLException;
-    
+
     /**
-     * Check whether a given ID already exists. 
+     * Check whether a given ID already exists.
+     *
      * @param ID
+     *
      * @return true if the ID exists.
-     * @throws java.sql.SQLException 
+     *
+     * @throws java.sql.SQLException
      */
     boolean checkForID(String ID) throws SQLException;
+
+    /**
+     * Get all the seats which are reserved in all the reservations on a certain
+     * flight
+     *
+     * @param flightID The flight to search for booked seats on.
+     */
+    void getAllBookedSeats(String flightID);
+    
+    /**
+     * Finds all the booked seats on a certain reservation.
+     * @param reservationID the reservation to find booked seats on.
+     */
+    void getBookedSeatsOnReservation(String reservationID);
 }
--- Flybooking/src/flybooking/ProgramStorage.java
+++ Flybooking/src/flybooking/ProgramStorage.java
@@ -56,7 +56,6 @@ public class ProgramStorage implements DatabaseInterface
         flightList.add(new Flight(750, 1, planeList.get(0), new Date(), new Date(), airportList.get(0), airportList.get(4)));
         flightList.add(new Flight(800, 1, planeList.get(0), new Date(), new Date(), airportList.get(0), airportList.get(4)));
         flightList.add(new Flight(400, 1, planeList.get(0), new Date(), new Date(), airportList.get(0), airportList.get(4)));
-
         // København til Moscow
         flightList.add(new Flight(1200, 1, planeList.get(0), new Timestamp(new Long("1386000000000")), new Timestamp(new Long("1388650000000")), airportList.get(0), airportList.get(3)));
         // København til Los Angelos
@@ -75,13 +74,6 @@ public class ProgramStorage implements DatabaseInterface
             }
         }
         return null; // Normally casts an exception here
-    }
-
-    @Override
-    public ArrayList<Seat> getTakenSeats(Flight flight, Person[] persons)
-    {
-        // måske ikke nødvendig
-        return null;
     }
 
     @Override
@@ -225,6 +217,18 @@ public class ProgramStorage implements DatabaseInterface
             }
         }
         return true;
+    }
+
+    @Override
+    public void getAllBookedSeats(String flightID)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getBookedSeatsOnReservation(String reservationID)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
