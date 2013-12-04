3ce67cd4052b39a776ca7a402f24d2f017e28e88
Anders Fischer-Nielsen <andersfischern@me.com>
2013-12-04 21:19:51 +0100

    Import-rettelse.

--- Flybooking/src/flybooking/GUI/EditReservationFrame.java
+++ Flybooking/src/flybooking/GUI/EditReservationFrame.java
@@ -4,7 +4,6 @@ package flybooking.GUI;
 import flybooking.*;
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.JFrame;
 import java.awt.event.*;
 import java.util.*;
 import net.miginfocom.swing.MigLayout;
