0589fe7370f4ccffcd1fbdb5cb9efc218c58d92c
Anders Fischer-Nielsen <andersfischern@me.com>
2013-12-05 11:34:10 +0100

    Kommentering af Seat.

--- Flybooking/src/flybooking/Seat.java
+++ Flybooking/src/flybooking/Seat.java
@@ -1,34 +1,44 @@
+
 package flybooking;
 
 /**
  * A plane seat.
+ *
  * @author Anders Wind Steffensen, Christoffer Forup & Anders Fischer-Nielsen
  */
-public class Seat
-{
+public class Seat {
 
+    //The ID of the seat.
     private final String ID;
+    //Whether the seat is available or not
     private boolean available;
     //What class the seat is. 0 = economy, 1 = business , 2 = firstclass. 
     private final int group;
 
     /**
-     * Construct a seat in a plane. It can be different kind of classes
-     * depending on the parameter group.
+     * Construct a seat in a plane. It can be different kind of class depending
+     * on the parameter "group".
      *
-     * @param ID        The ID of the seat, used for finding specific seats.
-     * @param group     The group this seat belongs to. 0 = economy, 1 =
-     *                  business , 2 = first class.
+     * @param ID The ID of the seat, used for finding specific seats.
+     * @param group The group this seat belongs to. 0 = economy, 1 = business, 2
+     * = first class.
      */
     public Seat(String ID, int group)
     {
         this.ID = ID;
         available = true;
-        this.group = group; // CHECK AT DEN ER 0, 1 ELLER 2.
+
+        //If the group ID is invalid set it to economy by default.
+        if (group < 3) {
+            this.group = group;
+        } else {
+            this.group = 0;
+        }
     }
 
     /**
      * Return whether the seat is available.
+     *
      * @return True if the seat is available, false if not.
      */
     public boolean getAvailability()
@@ -38,7 +48,8 @@ public class Seat
 
     /**
      * Set the availability of the seat.
-     * @param Whether the seat is available or not.
+     *
+     * @param available Whether the seat is available or not.
      */
     public void setAvailability(boolean available)
     {
@@ -46,9 +57,10 @@ public class Seat
     }
 
     /**
-     * Return what group the seat is in. 0 = economy, 1 = business, 
-     * 2 = first class.
-     * @return What group the seat is in. 0 = economy, 1 = business, 2 = first. 
+     * Return what group the seat is in. 0 = economy, 1 = business, 2 = first
+     * class.
+     *
+     * @return What group the seat is in. 0 = economy, 1 = business, 2 = first.
      */
     public int getGroup()
     {
@@ -57,6 +69,7 @@ public class Seat
 
     /**
      * Return the ID of the seat.
+     *
      * @return The ID of the seat.
      */
     public String getID()
